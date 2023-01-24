98639fa02062e3f24c8dd97bf4068c2af91ea267
Mohammed Javidh S <120474876+javidh0@users.noreply.github.com>
2023-01-24 21:52:55 +0530

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,2 +1,14 @@
+import pyrebase as pb
+
 class FireBase:
-    pass
+    __config = firebaseConfig = {
+    "apiKey": "AIzaSyBk-trGoXiH-alr7TXC9p8v6OXGBgWHrfE",
+    "authDomain": "attendance-management-8277c.firebaseapp.com",
+    "databaseURL": "https://attendance-management-8277c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "attendance-management-8277c",
+    "storageBucket": "attendance-management-8277c.appspot.com",
+    "messagingSenderId": "393696014722",
+    "appId": "1:393696014722:web:ac2d77466297c64c671f9b",
+    "measurementId": "G-Y019WT9CFF",
+    "databaseURL" : "https://attendance-management-8277c-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    }
