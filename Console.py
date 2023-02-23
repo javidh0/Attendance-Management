b2125d0c7812d74083f92392364fbf5bbe3fd1c3
Mohammed Javidh S <120474876+javidh0@users.noreply.github.com>
2023-02-23 09:57:40 +0530

    Consolw12

--- Console.py
+++ Console.py
@@ -14,6 +14,8 @@ class Console:
         pyg
         self.__FbObj = FireBase()
         self.__FbObj.initialize()
+
+        self.__root = Tk()
         self.__root.title("Console")
         
         width = self.__root.winfo_screenwidth() - 500   
@@ -29,6 +31,25 @@ class Console:
         self.__Sub = self.__FbObj.GetSubjectData()
 
     def MainWindow(self):
-        Label(self.__MainFrm, text="SRM-Attendance Console", font=self.__title).place(relx=0.5, rely=0.05)
+        Label(self.__MainFrm, text="SRM-Attendance Console", font=self.__title).place(relx=0.5, rely=0.05, anchor=CENTER)
         self.GetData()
+        fv1 = ['None']
+        fv2 = ['None']
+        fv3 = ['None']
+        for i in self.__Date.items():
+            fv1.append(i[0])
+        for i in self.__Fac.items():
+            fv2.append(i[0])
+        for i in self.__Sub.items():
+            fv3.append(i[0])
         
+        Label(self.__MainFrm, text="Search Attendance Record", font=self.__font).place(relx=0.02, rely=0.1)
+        self.cbdate = ttk.Combobox(self.__MainFrm, values=fv1, font=self.__font)
+        self.cbdate.place(relx=0.02, rely=0.15)
+        self.cbfac = ttk.Combobox(self.__MainFrm, values=fv2, font=self.__font)
+        self.cbfac.place(relx=0.02, rely=0.2)
+        self.cbsub = ttk.Combobox(self.__MainFrm, values=fv3, font=self.__font)
+        self.cbsub.place(relx=0.02, rely=0.25)
+
+    def enable(self):
+        self.__root.mainloop()
